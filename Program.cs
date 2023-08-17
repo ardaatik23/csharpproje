9aa0bbd57407eca00219efb39695b7cecd8d3e34
ardaatik23 <ardaatk0623@gmail.com>
2023-08-17 15:16:18 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int yarıcap = 5;
+
+        double cevresi = 2 * 3 * yarıcap;
+        double alan = 3 * yarıcap * yarıcap;
+
+        Console.WriteLine("Dairenin Çevresi: " + cevresi);
+        Console.WriteLine("Dairenin Alanı: " + alan);
+    }
+}
